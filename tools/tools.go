63e52ec89513e694c85906c3879f2a3ec54480bc
Chris Marget <cmarget@juniper.net>
2024-11-15 19:23:20 -0500

    collect third party licenses

---
+++ tools/tools.go
@@ -0,0 +1,20 @@
+//go:build tools
+
+package tools
+
+import (
+	// document generation
+	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
+
+	// license compliance
+	_ "github.com/google/go-licenses"
+
+	// staticcheck
+	_ "honnef.co/go/tools/cmd/staticcheck"
+
+	// release
+	_ "github.com/goreleaser/goreleaser"
+
+	// gofumpt does strict formatting
+	_ "mvdan.cc/gofumpt"
+)
